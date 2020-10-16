fbe3cd38a24b0d46458d384bbe0e961c6ebc7ec5
CrazyCanineStudios <32432850+CrazyCanineStudios@users.noreply.github.com>
2020-10-16 19:13:37 +0100

    Made the tree and cat change colour when hovering over so the player knows they are interactable

--- The-Last-Day/Assets/Scripts/Mini-Game/catPuzzle.cs
+++ The-Last-Day/Assets/Scripts/Mini-Game/catPuzzle.cs
@@ -11,21 +11,34 @@ namespace LastDay
         private bool catFalling;
         [SerializeField] private GameObject cat = null;
         [SerializeField] private Sprite [] catSprites = null;
-        public Timer timeTillCatFalls;
+        private Timer timeTillCatFalls;
+        public Timer timeTillCatFallsAnyway;
+
         // Update is called once per frame
         private void Start()
         {
             cat.GetComponent<Image>().sprite = catSprites[Random.Range(0, catSprites.Length - 1)];
             timeTillCatFalls = new Timer(3.4f);
+            timeTillCatFallsAnyway = new Timer(7f);
         }
         void Update()
         {
+            if (timeTillCatFallsAnyway.IsFinished)
+            {
+                catFalling = true;
+                cat.GetComponent<Rigidbody2D>().gravityScale = 10;
+            }
+            else
+            {
+                timeTillCatFallsAnyway.UpdateTimer(Time.deltaTime);
+            }
+
             if (numOfClicks >=4)
             {
                 catFalling = true;
                 cat.GetComponent<Rigidbody2D>().gravityScale = 10;
-                //miniGame.NextObjective();
             }
+
             if (catFalling)
             {
                 if (timeTillCatFalls.IsFinished)
